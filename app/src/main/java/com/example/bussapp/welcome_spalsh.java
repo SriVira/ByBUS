a5ecd0101a179f134a69af3483b0ac283d8acfe4
srivira <msriv1997@gmail.com>
2019-08-02 16:37:33 +0530

    Ganin Guttho

---
+++ app/src/main/java/com/example/bussapp/welcome_spalsh.java
@@ -0,0 +1,30 @@
+package com.example.bussapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class welcome_spalsh extends AppCompatActivity {
+
+
+    private static int Splash_Time = 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.welcome_splash);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent AdminPage = new Intent(welcome_spalsh.this,admin_dashboard.class);
+                    startActivity(AdminPage);
+                    finish();
+                }
+            },Splash_Time);
+
+        }
+    }
+
